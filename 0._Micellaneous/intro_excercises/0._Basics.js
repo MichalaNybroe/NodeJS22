05d1ed296779bc543a7881d85b6b321a9d0d48cb
Michala Nybroe <michalanim@gmail.com>
2022-08-29 10:34:13 +0200

    excercise one on basic js

---
+++ 0._Micellaneous/intro_excercises/0._Basics.js
@@ -0,0 +1,39 @@
+// --------------------------------------
+// Variables, strings, numbers
+// --------------------------------------
+// Exercise 1 - Console and constiables
+
+const firstName = "Anders";
+const lastName = "Latif";
+const total = 'My first name is ' + firstName  + ' and my last name is ' + lastName;
+//eller
+//`My first name is ${firstName} and my lastname is ${lastName}`
+// 'My first name is', firstname, "and my last name is", lastname
+
+
+// Print out the following in the console:
+// My first name is Anders and my last name is Latif
+
+console.log(total);
+
+// --------------------------------------
+// Exercise 2 - Numbers and Strings
+
+const year = "2021";
+const number = 1;
+
+const yearTotal = parseInt(year) + number;
+//eller
+//~~year + number
+// Number(year) + number
+// +year + number
+
+
+// Add the year plus the number
+// The result should be 2022
+// You cannot touch line 1 or 2
+
+console.log(yearTotal);
+
+// --------------------------------------
+
