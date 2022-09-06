851d28609932728edb20ce17573ad2600db36307
Michala Nybroe <michalanim@gmail.com>
2022-09-06 17:48:46 +0200

    rest api get weapon

--- Weapon/app.js
+++ Weapon/app.js
@@ -1,3 +1,20 @@
-const app = require => ("express") ();
+const app = require("express") ();
 
+const weapons = [ {name: "Knife", id: 1}, {name: "gun", id: 2}, {name: "canon", id: 3} ];
+
+app.get("/weapons", (req, res) => {
+    res.send({weapons});
+});
+
+app.get("/weapons/:id", (req, res) => {
+    const id = Number(req.params.id);
+    const weapon = weapons.find((weapon) => weapon.id === id)
+    console.log(weapon)
+
+    if (weapon !== undefined) {
+        res.send({weapon})
+    } else {
+        res.send({errorMessage: "No weapon with this id"});
+    }
+});
 app.listen(8080);
