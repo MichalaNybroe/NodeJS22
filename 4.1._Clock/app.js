f1e6fc6f87c9b75a8378e1daf51c1c49c1738b35
Michala Nybroe <michalanim@gmail.com>
2022-09-19 21:54:35 +0200

    start of clock assignment - ticking bomb countdown

---
+++ 4.1._Clock/app.js
@@ -0,0 +1,13 @@
+import express from "express";
+import path from "path";
+
+const app = express();
+
+app.get("/", (req, res) => {
+
+});
+
+
+
+
+app.listen(8080, () => {console.log("Server is running on port ", 8080)});
