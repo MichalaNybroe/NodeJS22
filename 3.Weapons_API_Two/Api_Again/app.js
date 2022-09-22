299755f144734f50595607e231758355d445a545
Michala Nybroe <michalanim@gmail.com>
2022-09-22 11:30:51 +0200

    new api for weapons

--- 3.Weapons_API_Two/Api_Again/app.js
+++ 3.Weapons_API_Two/Api_Again/app.js
@@ -27,22 +27,51 @@ app.post("/weapons", (req, res) => {
         const weapon = req.body;
         weapon.id = nextId++;
         weapons.push(weapon);
-        res.send({ data: weapon });
+        res.status(201).send({ data: weapon });
     } else {
         res.status(400).send({ data: undefined, message: "Name for weapon is required"})
     }
 });
 
 app.put("/weapons/:id", (req, res) => {
-
+    if (req.body.name !== undefined) {
+        const foundIndex = weapons.findIndex(weapon => weapon.id === Number(req.params.id));
+    
+        if (foundIndex !== -1) {
+            const foundWeapon = weapons[foundIndex];
+            const newWeapon = { ...foundWeapon, ...req.body, id: Number(req.params.id) };
+            weapons[foundIndex] = newWeapon;
+            res.status(200).send({ data: newWeapon })
+        } else {
+            res.status(404).send({ data: undefined, message: `No weapoon found by id: ${req.params.id}` });
+        }
+    } else {
+        res.status(400).send({data: undefined, message: "Weapon given in incorrect form"});
+    }
 });
 
 app.patch("/weapons/:id", (req, res) => {
+    const foundIndex = weapons.findIndex(weapon => weapon.id === Number(req.params.id));
 
+    if (foundIndex !== -1) {
+        const foundWeapon = weapons[foundIndex];
+        const weaponToUpdate = { ...foundWeapon, ...req.body, id: Number(req.params.id) };
+        weapons[foundIndex] = weaponToUpdate;
+        res.status(200).send({ data: weaponToUpdate })
+    } else {
+        res.status(404).send({ data: undefined, message: `No weapoon id found by id: ${req.params.id}` });
+    }
 });
 
 app.delete("/weapons/:id", (req, res) => {
+    const foundIndex = weapons.findIndex(weapon => weapon.id === Number(req.params.id));
 
+    if (foundIndex !== -1) {
+        const weapon = weapons.splice(foundIndex, 1)[0];
+        res.status(200).send({ data: weapon })
+    } else {
+        res.status(404).send({ data: undefined, message: `No weapoon id found by id: ${req.params.id}` });
+    }
 });
 
 app.listen(8080, () => console.log("Server is running on port ", 8080));
