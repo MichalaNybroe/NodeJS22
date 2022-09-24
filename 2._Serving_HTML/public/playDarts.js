a5214dae65394a5a85a5f5df2ef97b029230a9ea
Michala Nybroe <michalanim@gmail.com>
2022-09-24 16:35:31 +0200

    branch with only time excercise

--- 2._Serving_HTML/public/playDarts.js
+++
@@ -1,8 +0,0 @@
-// nyere syntaks så den importeres, hvorfor man kun skal henvise til en fil i html filen
-import {initializeScore, initializePlayers, initializeGame} from "./dartGame.js";
-
-initializeScore(501);
-
-initializePlayers("Simon", "Silke");
-
-initializeGame();
--- app.js
+++ app.js
@@ -6,11 +6,8 @@ const app = express();
 app.use(express.static("public"));
 
 app.get("/", (req, res) => {
-    res.sendFile(path.resolve("public/frontpage.html"))
+    res.sendFile(path.resolve("./public/frontpage.html"))
 });
 
 
-
-
-app.listen(8080, () => console.log("Server is running on port ", 8080));
-
+app.listen(8080, () => {console.log("Server is running on port ", 8080)});
