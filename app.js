4e2a0d118b950ba9ba724c01f2bb4d14450da5eb
Michala Nybroe <michalanim@gmail.com>
2022-09-24 16:48:34 +0200

    cant have fixed port use process.env.PORT || 8080

--- app.js
+++ app.js
@@ -10,4 +10,4 @@ app.get("/", (req, res) => {
 });
 
 
-app.listen(8080, () => {console.log("Server is running on port ", 8080)});
+app.listen(process.env.PORT || 8080, () => {console.log("Server is running on port ", 8080)});
