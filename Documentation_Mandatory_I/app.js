3c5981cd2cac3c86af7ff25e9edf1ed1b9fccc08
Michala Nybroe <michalanim@gmail.com>
2022-10-04 21:06:40 +0200

    basic pages added + ssr of same on endpoints

--- Documentation_Mandatory_I/app.js
+++ Documentation_Mandatory_I/app.js
@@ -10,23 +10,44 @@ app.use(express.json());
 
 //Pages
 const frontpage = renderPage("/frontpage/frontpage.html", { pageCss: `<link rel="stylesheet" href="./pages/frontpage/frontpage.css">`});
-const login = renderPage("/login/login.html", {tabTitle: "Login", pageCss: `<link rel="stylesheet" href="./pages/login/login.css">`});
-const signup = renderPage("/login/signup.html", {tabTitle: "Sign-Up", pageCss: `<link rel="stylesheet" href="./pages/login/signup.css">`});
+const loginPage = renderPage("/login/login.html", {tabTitle: "Login", pageCss: `<link rel="stylesheet" href="./pages/login/login.css">`});
+const signupPage = renderPage("/login/signup.html", {tabTitle: "Sign-Up", pageCss: `<link rel="stylesheet" href="./pages/login/signup.css">`});
+const setupPage = renderPage("/setup/setup.html", {tabTitle: "Set-Up NodeJs"});
+const triviaPage = renderPage("/trivia/trivia.html", {tabTitle: "NodeJs Fun!"});
+const commandPage = renderPage("commands/commands.html", {tabTitle: "NodeJs Commands"});
+const toolPage = renderPage("/tools/tools.html", {tabTitle: "NodeJs Tools"});
 
 app.get("/", (req, res) => {
     res.send(frontpage);
 });
 
 app.get("/login", (req, res) => {
-    res.send(login);
+    res.send(loginPage);
 });
 
 app.get("/signup", (req, res) => {
-    res.send(signup);
+    res.send(signupPage);
+});
+
+app.get("/setup", (req, res) => {
+    res.send(setupPage);
+});
+
+app.get("/trivia", (req, res) => {
+    res.send(triviaPage);
+});
+
+app.get("/commands", (req, res) => {
+    res.send(commandPage);
+});
+
+app.get("/tools", (req, res) => {
+    res.send(toolPage);
 });
 
 app.post("/login", (req, res) => {
     //Wait for form day 10/10
+    // make post for signup
 });
 
 
