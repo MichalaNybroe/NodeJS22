58566bca08bd889a8778986dcbc321952fcb8200
Michala Nybroe <michalanim@gmail.com>
2022-10-25 21:35:10 +0200

    parent child from silke notes

---
+++ 6._Svelte_Family/src/App.svelte
@@ -0,0 +1,39 @@
+<script>
+  import Home from "./components/Home.svelte";
+  import Parent from "./components/Parent.svelte";
+
+  const parents = ["Jørgen", "Andrea"];
+  const children = [
+    {
+      name: "Søren",
+      blackSheep: true
+    },
+    {
+      name: "Laura",
+      bright: true,
+      isGirl: true
+    },
+    {
+      name: "Anita",
+      bright: false
+    }
+  ];
+
+  const pets = [
+    { type: "Cat", name: "Nyx" },
+    { type: "Cat", name: "Athena" },
+    { type: "Dinosaur", name: "Rexie" }
+  ];
+
+</script>
+
+<main>
+  <Home>
+    <Parent name={parents[0]} children={children} pets={pets}/>
+    <Parent name={parents[1]} children={children} pets={pets}/>
+  </Home>
+</main>
+
+<style>
+ 
+</style>
---
+++ 6._Svelte_Family/src/components/Parent.svelte
@@ -0,0 +1,22 @@
+<script>
+    export let name;
+    export let children;
+    export let pets;
+
+    import Child from "./Child.svelte";
+    import Pet from "./Pet.svelte";
+</script>
+
+<h1>Hi, I'm {name}.</h1>
+
+
+<!--sådan man looper igennem børnene: -->
+{#each children as child}
+    <!--<p>{child.name}</p>-->
+    <!-- <p>{JSON.stringify(child)}</p> -->
+    <Child child={child}/>
+{/each}
+
+{#each pets as pet}
+    <Pet pet={pet}/>
+{/each}
