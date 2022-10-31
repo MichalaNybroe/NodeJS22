8822a24d14ac9b0800f4cca2cb9352b19842ec56
Michala Nybroe <michalanim@gmail.com>
2022-10-31 09:23:02 +0100

    send data from child to parent through function regerences

--- 6._Svelte_Family/src/components/Child.svelte
+++ 6._Svelte_Family/src/components/Child.svelte
@@ -1,5 +1,10 @@
 <script>
     export let child;
+    export let onTellILoveYou;
+    export let onAlwaysSayHi;
+    export let parentName;
 </script>
 
 <p>{child.name}</p>
+<button on:click={onTellILoveYou(`I love you, ${parentName}.`)}>Whisper in parents ear</button>
+<button on:click={onAlwaysSayHi}>Say Hi!</button>
--- 6._Svelte_Family/src/components/Parent.svelte
+++ 6._Svelte_Family/src/components/Parent.svelte
@@ -5,6 +5,15 @@
 
     import Child from "./Child.svelte";
     import Pet from "./Pet.svelte";
+
+    function handleTellILoveYou(message) { //konvention i react at sige handle
+        console.log(message);
+    };
+
+    function handleAlwaysSayHi(message) {
+        console.log(message);
+    }
+
 </script>
 
 <h1>Hi, I'm {name}.</h1>
@@ -14,7 +23,7 @@
 {#each children as child}
     <!--<p>{child.name}</p>-->
     <!-- <p>{JSON.stringify(child)}</p> -->
-    <Child child={child}/>
+    <Child child={child} onTellILoveYou={handleTellILoveYou} parentName={name} onAlwaysSayHi={handleAlwaysSayHi}/> <!--konvention at sige on i referance til handler-->
 {/each}
 
 {#each pets as pet}
