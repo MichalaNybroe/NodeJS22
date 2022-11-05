e256a7fea016e9a3bf6f3e0f9a9af0206fafd0bf
Michala Nybroe <michalanim@gmail.com>
2022-11-05 19:57:27 +0100

    change then in mail sending to message

--- 4.Pokemon/routers/contactRouter.js
+++ 4.Pokemon/routers/contactRouter.js
@@ -6,7 +6,7 @@ const router = Router();
 router.post("/api/contact", (req, res) => {
     console.log(req.body)
     sendMail(req.body.nameInput, req.body.emailInput,"Review Registration", "Your review has been registered. Thank you for your feedback.", "<p>Your review has been registered.</p>")
-    .then(() => res.send( { message: "Received review." }))
+    .then(() => res.send( { message: "An email confirmation has been send." }))
     .catch(() => res.status(400).send( { data: undefined, message: "Unsuccessful." } ));
 })
 
